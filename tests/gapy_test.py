fc15f1d14e9c7fa5d314cd5215a8c52404c81fac
pytlab <shaozhengjiang@gmail.com>
2017-07-19 16:06:28 +0800

    Add built-in roulette wheel seleciton test.

--- tests/gapy_test.py
+++ tests/gapy_test.py
@@ -8,11 +8,13 @@ import unittest
 
 from individual_test import IndividualTest
 from population_test import PopulationTest
+from roulette_wheel_selection_test import RouletteWheelSelectionTest
 
 def suite():
     test_suite = unittest.TestSuite([
         unittest.TestLoader().loadTestsFromTestCase(IndividualTest),
         unittest.TestLoader().loadTestsFromTestCase(PopulationTest),
+        unittest.TestLoader().loadTestsFromTestCase(RouletteWheelSelectionTest),
     ])
 
     return test_suite
---
+++ tests/roulette_wheel_selection_test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+''' Test case for built-in Roulette Wheel selection
+'''
+
+import unittest
+
+from gapy.components.population import GAPopulation
+from gapy.components.individual import GAIndividual
+from gapy.operators.builtin.selection.roulette_wheel import RouletteWheelSelection
+
+class RouletteWheelSelectionTest(unittest.TestCase):
+
+    def setUp(self):
+        self.maxDiff
+        def fitness(indv):
+            x, = indv.variants
+            return x**3 - 60*x**2 + 900*x + 100
+        self.fitness = fitness
+
+    def test_selection(self):
+        indv = GAIndividual(ranges=[(0, 30)])
+        p = GAPopulation(indv)
+        p.init()
+
+        selection = RouletteWheelSelection(self.fitness)
+        father, mother = selection.select(p)
+
+        self.assertTrue(isinstance(father, GAIndividual))
+        self.assertTrue(isinstance(mother, GAIndividual))
+        self.assertNotEqual(father.chromsome, mother.chromsome)
+
+if '__main__' == __name__:
+    suite = unittest.TestLoader().loadTestsFromTestCase(RouletteWheelSelectionTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
