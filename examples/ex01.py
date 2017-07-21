2daabb8b75c5977db4fe289fc2fcc58ab0647e4e
pytlab <shaozhengjiang@gmail.com>
2017-07-21 16:45:59 +0800

    Add analysis definition.

--- examples/ex01.py
+++ examples/ex01.py
@@ -13,7 +13,9 @@ from gapy.components import GAPopulation
 from gapy.operators import RouletteWheelSelection
 from gapy.operators import UniformCrossover
 from gapy.operators import FlipBitMutation
-from gapy.analysis import ConsoleOutputAnalysis
+
+# Analysis plugin base class.
+from gapy.plugin_interfaces.analysis import OnTheFlyAnalysis
 
 # Define population.
 indv_template = GAIndividual(ranges=[(0, 10)], encoding='binary', eps=0.001)
@@ -26,7 +28,7 @@ mutation = FlipBitMutation(pm=0.1)
 
 # Create genetic algorithm engine.
 engine = GAEngine(population=population, selection=selection,
-                  crossover=crossover, mutation=mutation, analysis=[ConsoleOutputAnalysis])
+                  crossover=crossover, mutation=mutation)
 
 # Define fitness function.
 @engine.fitness_register
@@ -34,7 +36,16 @@ def fitness(indv):
     x, = indv.variants
     return x + 10*sin(5*x) + 7*cos(4*x)
 
+# Define on-the-fly analysis.
+@engine.analysis_register
+class ConsoleOutputAnalysis(OnTheFlyAnalysis):
+    interval = 1
+    def register_step(self, ng, population, engine):
+        best_indv = population.best_indv(engine.fitness)
+        msg = 'Generation: {}, best fitness: {:.3f}'.format(ng, engine.fitness(best_indv))
+        engine.logger.info(msg)
 
 if '__main__' == __name__:
+    # Run the GA engine.
     engine.run(ng=100)
 
