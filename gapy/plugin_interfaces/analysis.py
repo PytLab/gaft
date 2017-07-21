49135ba22d64e32ad50732aa66ec661cef16a3a7
pytlab <shaozhengjiang@gmail.com>
2017-07-21 15:17:39 +0800

    Add metaclass for OnTheFlyAnalysis class.

--- gapy/plugin_interfaces/analysis.py
+++ gapy/plugin_interfaces/analysis.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class OnTheFlyAnalysis(object):
+class OnTheFlyAnalysis(metaclass=AnalysisMeta):
     '''
     Class for providing an interface to easily extend and customize the behavior
     of the on-the-fly analysis functionality of gapy.
     '''
 
-    def __init__(self, interval):
+    # Analysis interval.
+    interval = 1
+
+    def __init__(self, *kwargs):
         '''
         The constructor of the base-class.
-
-        :param interval: analysis interval.
-        :type interval: int
         '''
-        self.interval = interval
+        pass
 
-    def setup(self, population, engine):
+    def setup(self, population, engine, **kwargs):
         '''
         Function called right before the start of genetic algorithm main iteration
         to allow for custom setup of the analysis object.
@@ -26,10 +26,12 @@ class OnTheFlyAnalysis(object):
 
         :param engine: The current GAEngine where the analysis is running.
         :type engine: GAEngine
+
+        **You can also add more custom parameters to this method.**
         '''
         raise NotImplementedError
 
-    def register_step(self, ng, population, engine):
+    def register_step(self, ng, population, engine, **kwargs):
         '''
         Function called in each iteration step.
 
@@ -41,6 +43,8 @@ class OnTheFlyAnalysis(object):
 
         :param engine: The current GAEngine where the analysis is running.
         :type engine: GAEngine
+
+        **You can also add more custom parameters to this method.**
         '''
         raise NotImplementedError
 
@@ -50,4 +54,28 @@ class OnTheFlyAnalysis(object):
         post-processing of the collected data.
         '''
         raise NotImplementedError
+
+
+class AnalysisMeta(type):
+    '''
+    Metaclass for analysis plugin class
+    '''
+    @classmethod
+    def __prepare__(cls, name, bases, **kwargs):
+        return {}
+
+    def __new__(cls, name, bases, attrs):
+        # Check interval type.
+        if 'interval' in attrs:
+            interval = attrs['interval']
+            if type(interval) is not int or interval <= 0:
+                raise TypeError('analysis interval must be a positive integer')
+
+        for method_name in ['setup', 'register_step', 'finalize']:
+            method = attrs.get(method_name, None)
+            if not callable(method):
+                msg = "'setup', 'register_step' and 'finalize' must be defiend as method"
+                raise AttributeError(msg)
+
+        return cls
 
