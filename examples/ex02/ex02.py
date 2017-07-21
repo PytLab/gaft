bc34dac3fb485e19349951357b60326fbcf6aa8a
pytlab <shaozhengjiang@gmail.com>
2017-07-22 00:21:03 +0800

    Add new example.

---
+++ examples/ex02/ex02.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Find the global maximum for binary function: f(x) = y*sim(2*pi*x) + x*cos(2*pi*y)
+'''
+
+from math import sin, cos, pi
+
+from gaft import GAEngine
+from gaft.components import GAIndividual
+from gaft.components import GAPopulation
+from gaft.operators import RouletteWheelSelection
+from gaft.operators import UniformCrossover
+from gaft.operators import FlipBitMutation
+
+# Built-in best fitness analysis.
+from gaft.analysis.fitness_store import FitnessStoreAnalysis
+from gaft.analysis.console_output import ConsoleOutputAnalysis
+
+# Define population.
+indv_template = GAIndividual(ranges=[(-2, 2), (-2, 2)], encoding='binary', eps=0.001)
+population = GAPopulation(indv_template=indv_template, size=50)
+
+# Create genetic operators.
+selection = RouletteWheelSelection()
+crossover = UniformCrossover(pc=0.8, pe=0.5)
+mutation = FlipBitMutation(pm=0.1)
+
+# Create genetic algorithm engine.
+# Here we pass all built-in analysis to engine constructor.
+engine = GAEngine(population=population, selection=selection,
+                  crossover=crossover, mutation=mutation,
+                  analysis=[ConsoleOutputAnalysis, FitnessStoreAnalysis])
+
+# Define fitness function.
+@engine.fitness_register
+def fitness(indv):
+    x, y = indv.variants
+    return y*sin(2*pi*x) + x*cos(2*pi*y)
+
+if '__main__' == __name__:
+    engine.run(ng=100)
+
