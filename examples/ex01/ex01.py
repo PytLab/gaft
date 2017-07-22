59785b895330e124598f348075c3357122c51872
pytlab <shaozhengjiang@gmail.com>
2017-07-22 10:31:06 +0800

    Add original function curve.

--- examples/ex01/ex01.py
+++ examples/ex01/ex01.py
@@ -44,9 +44,17 @@ def fitness(indv):
 @engine.analysis_register
 class ConsoleOutputAnalysis(OnTheFlyAnalysis):
     interval = 1
+
     def register_step(self, ng, population, engine):
         best_indv = population.best_indv(engine.fitness)
         msg = 'Generation: {}, best fitness: {:.3f}'.format(ng, engine.fitness(best_indv))
+        engine.logger.info(msg)
+
+    def finalize(self, population, engine):
+        best_indv = population.best_indv(engine.fitness)
+        x = best_indv.variants
+        y = engine.fitness(best_indv)
+        msg = 'Optimal solution: ({}, {})'.format(x, y)
         engine.logger.info(msg)
 
 if '__main__' == __name__:
--- examples/ex01/plot_envolution_curve.py
+++ examples/ex01/plot_envolution_curve.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from math import sin, cos
+import numpy as np
+import matplotlib.pyplot as plt
 from best_fit import best_fit
 
-import matplotlib.pyplot as plt
-
-steps, fits = list(zip(*best_fit))
+steps, variants, fits = list(zip(*best_fit))
+best_step, best_v, best_f = steps[-1], variants[-1][0], fits[-1]
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
+
+ax = fig.add_subplot(211)
+f = lambda x: x + 10*sin(5*x) + 7*cos(4*x)
+x = np.linspace(0, 10, 1000)
+y = [f(i) for i in x]
+ax.plot(x, y)
+ax.scatter([best_v], [best_f], facecolor='r')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+
+ax = fig.add_subplot(212)
 ax.plot(steps, fits)
-ax.set_xlabel('generation')
-ax.set_ylabel('fitness')
+ax.set_xlabel('Generation')
+ax.set_ylabel('Fitness')
 
 # Plot the maximum.
-x, y = steps[-1], fits[-1]
-ax.scatter([x], [y], facecolor='r')
-ax.annotate(s='x: {}\ny:{:.2f}'.format(x, y), xy=(x, y), xytext=(x-0.3, y-0.3))
+ax.scatter([best_step], [best_f], facecolor='r')
+ax.annotate(s='x: {:.2f}\ny:{:.2f}'.format(best_v, best_f),
+                                           xy=(best_step, best_f),
+                                           xytext=(best_step-0.3, best_f-0.3))
+
 
 plt.show()
 
