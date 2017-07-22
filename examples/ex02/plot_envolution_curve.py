b98ebd4fb667990c29706693162c3d4dfc48b137
pytlab <shaozhengjiang@gmail.com>
2017-07-22 21:05:34 +0800

    Add surface plot.

---
+++ examples/ex02/plot_envolution_curve.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from math import sin, cos
+import numpy as np
+import matplotlib.pyplot as plt
+from best_fit import best_fit
+
+steps, variants, fits = list(zip(*best_fit))
+best_step, best_v, best_f = steps[-1], variants[-1], fits[-1]
+
+fig = plt.figure()
+
+ax = fig.add_subplot(111)
+ax.plot(steps, fits)
+ax.set_xlabel('Generation')
+ax.set_ylabel('Fitness')
+
+# Plot the maximum.
+ax.scatter([best_step], [best_f], facecolor='r')
+ax.annotate(s='x: [{:.2f}, {:.2f}]\ny:{:.2f}'.format(*best_v, best_f),
+                                                     xy=(best_step, best_f),
+                                                     xytext=(best_step, best_f-0.1))
+
+
+plt.show()
+
---
+++ examples/ex02/plot_surf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import mpl_toolkits.mplot3d
+import matplotlib.pyplot as plt
+
+from best_fit import best_fit
+
+for i, (x, y), z in best_fit:
+    fig = plt.figure(figsize=(12, 8))
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter([x], [y], [z], zorder=99, c='r', s=100)
+
+    x, y = np.mgrid[-2:2:100j, -2:2:100j]
+    z = y*np.sin(2*np.pi*x) + x*np.cos(2*np.pi*y)
+    ax.plot_surface(x, y, z, rstride=2, cstride=1, cmap=plt.cm.bone_r)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    fig.savefig('{}.png'.format(i))
+    print('save {}.png'.format(i))
+    plt.close(fig)
+
