bee4fb50bc0c0190edba714b2c1b21fa4c9aa930
pytlab <shaozhengjiang@gmail.com>
2017-09-18 15:37:26 +0800

    Use binary tournament selection operator.

--- examples/ex01/ex01.py
+++ examples/ex01/ex01.py
@@ -10,7 +10,7 @@ from math import sin, cos
 from gaft import GAEngine
 from gaft.components import GAIndividual
 from gaft.components import GAPopulation
-from gaft.operators import RouletteWheelSelection
+from gaft.operators import RouletteWheelSelection, TournamentSelection
 from gaft.operators import UniformCrossover
 from gaft.operators import FlipBitMutation
 
@@ -25,7 +25,8 @@ indv_template = GAIndividual(ranges=[(0, 10)], encoding='binary', eps=0.001)
 population = GAPopulation(indv_template=indv_template, size=50).init()
 
 # Create genetic operators.
-selection = RouletteWheelSelection()
+#selection = RouletteWheelSelection()
+selection = TournamentSelection()
 crossover = UniformCrossover(pc=0.8, pe=0.5)
 mutation = FlipBitMutation(pm=0.1)
 
