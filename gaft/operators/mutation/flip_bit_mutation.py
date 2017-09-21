554a816436b8509f33c6275b99088e3bfe5d049a
pytlab <shaozhengjiang@gmail.com>
2017-09-21 16:17:18 +0800

    Add new flip bit mutation with big mutation rate.

--- gaft/operators/mutation/flip_bit_mutation.py
+++ gaft/operators/mutation/flip_bit_mutation.py
@@ -19,7 +19,7 @@ class FlipBitMutation(GAMutation):
 
         self.pm = pm
 
-    def mutate(self, individual):
+    def mutate(self, individual, engine):
         '''
         Mutate the individual.
         '''
@@ -35,4 +35,53 @@ class FlipBitMutation(GAMutation):
             individual.variants = individual.decode()
 
         return individual
+
+
+class FlipBitBigMutation(FlipBitMutation):
+    def __init__(self, pm, pbm, alpha):
+        '''
+        Mutation operator using Flip Bit mutation implementation with adaptive
+        big mutation rate to overcome premature or local-best solution.
+
+        :param pm: The probability of mutation (usually between 0.001 ~ 0.1)
+        :type pm: float in (0.0, 1.0]
+
+        :param pbm: The probability of big mutation, usually more than 5 times
+                    bigger than pm.
+        :type pbm: float
+
+        :param alpha: intensive factor
+        :type alpha: float, in range (0.5, 1)
+        '''
+        super(self.__class__, self).__init__(pm)
+
+        if not (0.0 < pbm < 1.0):
+            raise ValueError('Invalid big mutation probability')
+        if pbm < 5*pm:
+            self.logger.warning('Relative low probability for big mutation')
+        self.pbm = pbm
+
+        # Intensive factor.
+        if not (0.5 < alpha < 1.0):
+            raise ValueError('Invalid intensive factor, should be in (0.5, 1.0)')
+        self.alpha = alpha
+
+    def mutate(self, individual, engine):
+        '''
+        Mutate the individual with adaptive big mutation rate.
+        '''
+        population, fitness = engine.population, engine.fitness
+
+        fmean = population.fmean(fitness)
+        fmax = population.fmax(fitness)
+
+        if fmax*self.alpha < fmean:
+            pm = self.pm
+            self.pm = self.pbm
+            # Mutate with big probability.
+            individial = super(self.__class__, self).mutate(individial, engine)
+            # Recover probability.
+            self.pm = pm
+
+        return individial
 
