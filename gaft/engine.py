96b9f666c5741d771eb3b32f7bc7cfa8bdb0504d
pytlab <shaozhengjiang@gmail.com>
2017-10-06 22:01:10 +0800

    Add profiling decorator.

--- gaft/engine.py
+++ gaft/engine.py
@@ -7,10 +7,46 @@ import logging
 import math
 from functools import wraps
 
+# Imports for profiling.
+import cProfile
+import pstats
+import os
+
 from .components import GAIndividual, GAPopulation
 from .plugin_interfaces.operators import GASelection, GACrossover, GAMutation
 from .plugin_interfaces.analysis import OnTheFlyAnalysis
 from .mpiutil import mpi
+
+
+def do_profile(filename, sortby='tottime'):
+    '''
+    Constructor for function profiling decorator.
+    '''
+    def _do_profile(func):
+        '''
+        Function profiling decorator.
+        '''
+        @wraps(func)
+        def profiled_func(*args, **kwargs):
+            '''
+            Decorated function.
+            '''
+            # Flag for doing profiling or not.
+            DO_PROF = os.getenv('PROFILING')
+
+            if DO_PROF:
+                profile = cProfile.Profile()
+                profile.enable()
+                result = func(*args, **kwargs)
+                profile.disable()
+                ps = pstats.Stats(profile).sort_stats(sortby)
+                ps.dump_stats(filename)
+            else:
+                result = func(*args, **kwargs)
+            return result
+        return profiled_func
+
+    return _do_profile
 
 
 class StatVar(object):
@@ -104,6 +140,7 @@ class GAEngine(object):
         # Check parameters validity.
         self._check_parameters()
 
+    @do_profile(filename='gaft_run.prof')
     def run(self, ng=100):
         '''
         Run the Genetic Algorithm optimization iteration with specified parameters.
