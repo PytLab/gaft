1ce4d08a6fe39fc393ac68967b58f29ca4b42658
pytlab <shaozhengjiang@gmail.com>
2017-12-15 10:35:14 +0800

    Add engine running test for decimal encoding individuals.

--- tests/engine_test.py
+++ tests/engine_test.py
@@ -8,7 +8,7 @@ import unittest
 from math import sin, cos
 
 from gaft import GAEngine
-from gaft.components import BinaryIndividual
+from gaft.components import BinaryIndividual, DecimalIndividual
 from gaft.components import Population
 from gaft.operators import RouletteWheelSelection
 from gaft.operators import UniformCrossover
@@ -20,11 +20,35 @@ class GAEngineTest(unittest.TestCase):
     def setUp(self):
         self.maxDiff = True
 
-    def test_run(self):
+    def test_binary_run(self):
         '''
         Make sure GA engine can run correctly.
         '''
         indv_template = BinaryIndividual(ranges=[(0, 10)], eps=0.001)
+        population = Population(indv_template=indv_template, size=50).init()
+
+        # Create genetic operators.
+        selection = RouletteWheelSelection()
+        crossover = UniformCrossover(pc=0.8, pe=0.5)
+        mutation = FlipBitMutation(pm=0.1)
+
+        # Create genetic algorithm engine.
+        engine = GAEngine(population=population, selection=selection,
+                          crossover=crossover, mutation=mutation)
+
+        @engine.fitness_register
+        @engine.minimize
+        def fitness(indv):
+            x, = indv.solution
+            return x + 10*sin(5*x) + 7*cos(4*x)
+
+        engine.run(50)
+
+    def test_decimal_run(self):
+        '''
+        Make sure GA engine can run correctly with decimal encoding individuals.
+        '''
+        indv_template = DecimalIndividual(ranges=[(0, 10)], eps=0.001)
         population = Population(indv_template=indv_template, size=50).init()
 
         # Create genetic operators.
